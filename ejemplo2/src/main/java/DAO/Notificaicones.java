5266af77c044a7f46f98b09c523581a7a83a38ff
APRENDIZ <APRENDIZ@STMAPRRMDFSP603.FormacionSena.RED>
2025-09-09 19:10:47 -0500

    Actualización #8 del proyecto ejemplo2 Steban Pineda Peñuela, Carlos Gómez Ayala

---
+++ ejemplo2/src/main/java/DAO/Notificaicones.java
@@ -0,0 +1,62 @@
+package DAO;
+
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+public class Notificaicones {
+	// Método que envía una notificación por correo
+	public void SIM(String asunto, String mensajeTexto) {
+
+		// ...
+
+		// Tu cuenta de Gmail (remitente)
+		String remitente = "caligomez294@gmail.com";
+		String clave = "cqec dtwz unny xiia"; // Contraseña de aplicación, no la personal
+
+		String receptor = "steban19pin@gmail.com";
+
+		// Configuración SMTP //Protocolo de transferencia de correo
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		// Crear sesión autenticada con credenciales
+		Session session = Session.getInstance(props, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(remitente, clave);
+			}
+		});
+
+		try {
+			// Crear mensaje
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(remitente));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receptor));
+			message.setSubject(asunto);
+			message.setText(mensajeTexto);
+
+			// Enviar
+			Transport.send(message);
+
+			System.out.println("correo enviado");
+
+			// Mostrar mensaje en respuesta
+
+		} catch (MessagingException e) {
+			// Mostrar error
+
+		}
+	}
+
+}
